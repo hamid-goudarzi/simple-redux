20b3e159ad26073f63126aa882310353a22eae90
Hamid Goudarzi <goudarzi.hamid@gmail.com>
2020-07-15 13:47:14 -0400

    Updates

---
+++ precache-manifest.75e2f2623a5089e0ee7b272f0f6cbb65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5ad544a643d2a0080ddcf68519e5069",
+    "url": "/simple-redux/index.html"
+  },
+  {
+    "revision": "8f49196c196576baff50",
+    "url": "/simple-redux/static/js/2.9320a8c6.chunk.js"
+  },
+  {
+    "revision": "3b0760bece22b550433845f448e395fa",
+    "url": "/simple-redux/static/js/2.9320a8c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90710128f4ce44fa25c2",
+    "url": "/simple-redux/static/js/main.5b9d7858.chunk.js"
+  },
+  {
+    "revision": "ed235415bb7334e08762",
+    "url": "/simple-redux/static/js/runtime-main.67b4749c.js"
+  }
+]);
